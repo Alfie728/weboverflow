2a99198614ff29165c2788413dc36b94c8dc5f3b
Alfie728 <zegro728@gmail.com>
2024-06-14 00:26:59 +1000

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/ask-question"]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
+export default clerkMiddleware({ debug: true });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
