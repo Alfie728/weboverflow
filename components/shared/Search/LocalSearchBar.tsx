2541f275c2edff2b5b3c1e1561973fb0ed987065
Alfie728 <zegro728@gmail.com>
2024-07-13 21:45:34 +1000

    managed search state in URL in LocalSearchBar

--- components/shared/Search/LocalSearchBar.tsx
+++ components/shared/Search/LocalSearchBar.tsx
@@ -2,6 +2,9 @@
 
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 
 interface CustomInputProps {
   route: string;
@@ -18,6 +21,41 @@ const LocalSearchBar = ({
   placeholder,
   otherClasses,
 }: CustomInputProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const query = searchParams.get("q");
+
+  const [search, setSearch] = useState(query || "");
+  console.log(search);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "q",
+          value: search,
+        });
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === route) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["q"],
+          });
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 500);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, route, pathname, router, searchParams, query]);
+
+  console.log(query);
   return (
     <div
       className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
@@ -35,9 +73,9 @@ const LocalSearchBar = ({
       <Input
         type="text"
         placeholder={placeholder}
-        value=""
-        onChange={() => {}}
-        className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="paragraph-regular no-focus placeholder background-light800_darkgradient text-dark400_light800 border-none shadow-none outline-none"
       />
 
       {iconPosition === "right" && (
