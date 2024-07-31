5da205ad8aa63dd631d4ba4c67696d6a532e974a
Alfie728 <zegro728@gmail.com>
2024-07-31 22:18:27 +1000

    added Pagination on homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -8,12 +8,13 @@ import QuestionCard from "@/components/cards/QuestionCard";
 import NoResult from "@/components/shared/NoResult";
 import { getQuestions } from "@/lib/actions/question.action";
 import { SearchParamsProps } from "@/types";
+import Pagination from "@/components/shared/Pagination";
 
 export default async function Home({ searchParams }: SearchParamsProps) {
-  // console.log(searchParams);
   const result = await getQuestions({
     searchQuery: searchParams.q,
     filter: searchParams.filter,
+    page: searchParams.page ? +searchParams.page : 1,
   });
 
   return (
@@ -69,6 +70,12 @@ export default async function Home({ searchParams }: SearchParamsProps) {
             linkTitle="Ask a question"
           />
         )}
+      </div>
+      <div className="mt-10">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        />
       </div>
     </>
   );
--- components/shared/Sidebar/LeftSidebar.tsx
+++ components/shared/Sidebar/LeftSidebar.tsx
@@ -13,8 +13,8 @@ const LeftSidebar = () => {
   console.log(userId);
 
   return (
-    <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
-      <div className="flex flex-1 flex-col gap-6">
+    <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-around border-r p-6 shadow-light-300 dark:shadow-none max-lg:p-3 max-sm:hidden ">
+      <div className="flex w-full flex-1 flex-col gap-6 pt-[200px] max-lg:pt-[212px]">
         {sidebarLinks.map((item) => {
           const isActive =
             (pathname.includes(item.route) && item.route.length > 1) ||
