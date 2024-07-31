41c4cafc895af1441ee03454687321f9c5725e25
Alfie728 <zegro728@gmail.com>
2024-07-31 22:46:04 +1000

    fixed layout shift caused by react-remove-scroll

--- components/shared/Navbar/Navbar.tsx
+++ components/shared/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ import GlobalSearch from "../Search/GlobalSearch";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-[calc(100%-var(--removed-body-scroll-bar-size,0px))]  gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
