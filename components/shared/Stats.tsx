1146b5bf177c57fef9c919301fd2df274a6ed9b9
Alfie728 <zegro728@gmail.com>
2024-08-15 23:27:21 +1000

    statsCard padding fix, container query fix

--- components/shared/Stats.tsx
+++ components/shared/Stats.tsx
@@ -15,7 +15,7 @@ const StatsCard = ({ imgURL, value, title }: StatsCardProps) => {
   return (
     <article className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-4 rounded-md border p-6 shadow-light-300 @container dark:shadow-dark-200">
       <Image src={imgURL} alt={title} width={40} height={50} />
-      <div className="grid place-items-center @[10rem]:flex @[10rem]:gap-4">
+      <div className="flex flex-col items-center justify-center @[10rem]:flex-row @[10rem]:gap-4">
         <p className="paragraph-semibold text-dark200_light900">{value}</p>
         <p className="body-medium text-dark400_light700">{title}</p>
       </div>
@@ -28,14 +28,14 @@ const Stats = ({ totalQuestions, totalAnswers }: Props) => {
     <div className="mt-10">
       <h4 className="text-dark400_light900 h3-semibold">Stats</h4>
       <div className="mt-5 grid grid-cols-[repeat(auto-fit,minmax(10rem,1fr))] gap-4">
-        <article className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-5 rounded-md border p-4 shadow-light-300 @container dark:shadow-dark-200">
-          <div className="grid place-items-center @[10rem]:flex @[10rem]:gap-4">
+        <article className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-5 rounded-md border p-6 pb-[1.125rem] pt-7 shadow-light-300 @container dark:shadow-dark-200">
+          <div className="flex flex-col items-center @[10rem]:flex-row @[10rem]:gap-4">
             <p className="paragraph-semibold text-dark200_light900">
               {totalQuestions}
             </p>
             <p className="body-medium text-dark400_light700">Questions</p>
           </div>
-          <div className="grid place-items-center @[10rem]:flex @[10rem]:gap-4">
+          <div className="flex flex-col items-center @[10rem]:flex-row @[10rem]:gap-4">
             <p className="paragraph-semibold text-dark200_light900">
               {totalAnswers}
             </p>
