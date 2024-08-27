53165dc19206e099588d102dc6face6ee6fa6998
Alfie728 <zegro728@gmail.com>
2024-08-27 22:48:30 +1000

    added badge system & style fix

--- components/shared/Stats.tsx
+++ components/shared/Stats.tsx
@@ -1,8 +1,11 @@
+import { BadgeCounts } from "@/types";
 import Image from "next/image";
 
 interface Props {
   totalQuestions: number;
   totalAnswers: number;
+  badges: BadgeCounts;
+  reputation: number;
 }
 
 interface StatsCardProps {
@@ -13,30 +16,32 @@ interface StatsCardProps {
 
 const StatsCard = ({ imgURL, value, title }: StatsCardProps) => {
   return (
-    <article className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-4 rounded-md border p-6 shadow-light-300 @container dark:shadow-dark-200">
+    <article className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-4 rounded-md border p-6 transition @container hover:shadow-lg dark:shadow-light-100 dark:hover:shadow-light-200">
       <Image src={imgURL} alt={title} width={40} height={50} />
-      <div className="flex flex-col items-center justify-center @[10rem]:flex-row @[10rem]:gap-4">
-        <p className="paragraph-semibold text-dark200_light900">{value}</p>
-        <p className="body-medium text-dark400_light700">{title}</p>
+      <div className="flex w-full flex-col items-center justify-center @[10rem]:flex-row @[10rem]:gap-4">
+        <p className="paragraph-semibold text-primary-500">{value}</p>
+        <p className="body-medium text-dark200_light900">{title}</p>
       </div>
     </article>
   );
 };
 
-const Stats = ({ totalQuestions, totalAnswers }: Props) => {
+const Stats = ({ totalQuestions, totalAnswers, badges, reputation }: Props) => {
   return (
     <div className="mt-10">
-      <h4 className="text-dark400_light900 h3-semibold">Stats</h4>
+      <h4 className="h3-semibold text-dark200_light900">
+        Reputation - <span className="text-primary-500">{reputation}</span>
+      </h4>
       <div className="mt-5 grid grid-cols-[repeat(auto-fit,minmax(10rem,1fr))] gap-4">
-        <article className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-5 rounded-md border p-6 pb-[1.125rem] pt-7 shadow-light-300 @container dark:shadow-dark-200">
-          <div className="flex flex-col items-center @[10rem]:flex-row @[10rem]:gap-4">
-            <p className="paragraph-semibold text-dark200_light900">
+        <article className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-5 rounded-md border p-6 pb-[1.125rem] pt-7 @container  hover:shadow-lg dark:shadow-light-100 dark:hover:shadow-light-200">
+          <div className="flex w-full flex-col items-center justify-center @[10rem]:flex-row @[10rem]:gap-4">
+            <p className="paragraph-semibold text-primary-500">
               {totalQuestions}
             </p>
             <p className="body-medium text-dark400_light700">Questions</p>
           </div>
-          <div className="flex flex-col items-center @[10rem]:flex-row @[10rem]:gap-4">
-            <p className="paragraph-semibold text-dark200_light900">
+          <div className="flex w-full flex-col items-center justify-center @[10rem]:flex-row @[10rem]:gap-4">
+            <p className="paragraph-semibold text-primary-500">
               {totalAnswers}
             </p>
             <p className="body-medium text-dark400_light700">Answers</p>
@@ -45,18 +50,18 @@ const Stats = ({ totalQuestions, totalAnswers }: Props) => {
 
         <StatsCard
           imgURL="/assets/icons/gold-medal.svg"
-          value={0}
+          value={badges.GOLD}
           title="Gold Badges"
         />
         <StatsCard
           imgURL="/assets/icons/silver-medal.svg"
-          value={0}
-          title="Gold Badges"
+          value={badges.SILVER}
+          title="Sliver Badges"
         />
         <StatsCard
           imgURL="/assets/icons/bronze-medal.svg"
-          value={0}
-          title="Gold Badges"
+          value={badges.BRONZE}
+          title="Bronze Badges"
         />
       </div>
     </div>
